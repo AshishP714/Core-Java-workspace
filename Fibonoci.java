2d3f78b0e726b3ab342e49c7873de72f10bccd18
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-03-24 14:28:41 +0530

    Add files via upload

---
+++ Fibonoci.java
@@ -0,0 +1,19 @@
+package CollectionFramewor;
+
+public class Fibonoci {
+
+	public static void main(String[] args) {
+		
+		int num = 1;
+		int temp = 1;
+//		1 1 2 3 5 8
+		
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(num);
+
+			int count = num + temp;
+			num = temp;
+			temp = count;
+		}
+	}
+} 
