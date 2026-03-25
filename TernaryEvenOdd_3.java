e8f1e103695ff304a7352ffcbe5d10d7bec8a42c
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-03-25 10:42:11 +0530

    Add files via upload

---
+++ TernaryEvenOdd_3.java
@@ -0,0 +1,13 @@
+package Exercise;
+
+public class TernaryEvenOdd_3 {
+
+	public static void main(String[] args) {
+		
+		int num = 5;
+		
+		String result = (num % 2 == 0) ? "Even" : "Odd";
+		
+		System.out.println(result);
+	}
+}
