19e135b54f1c0033bcf0b8a2cd6d0dde086209d6
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-01 21:32:35 +0530

    Add files via upload

---
+++ Excercise/FindSecondHigestInArray.java
@@ -0,0 +1,28 @@
+package Exercise;
+
+public class FindSecondHigestInArray {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 2, 4, 5, 6, 67, 88 };
+		int max = arr[0];
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		
+		System.out.println(max);
+		
+		int secondHigest = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] < max) {
+				secondHigest = arr[i];
+			}
+		}
+		
+		System.out.println(secondHigest);
+	}
+}
