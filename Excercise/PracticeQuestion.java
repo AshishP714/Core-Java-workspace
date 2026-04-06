144688a19061235fc7497a60b7e89bf110e4d7bd
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-06 08:25:00 +0530

    Update PracticeQuestion.java

--- Excercise/PracticeQuestion.java
+++ Excercise/PracticeQuestion.java
@@ -2,9 +2,9 @@ package com.string;
 
 public class PracticeQuestion {
 public static void main(String[] args) {
-	String s="payal";
-	String s1=new String ("chavan");
-	String s2=s.concat("chavan");
+	String s="Ashish";
+	String s1=new String ("Pardeshi");
+	String s2=s.concat("Pardeshi");
 	System.out.println(s==s1);
 	System.out.println(s1==s2);
 }
