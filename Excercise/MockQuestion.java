80e4b862fe0e32abd68bedf4255ed7ee899169d2
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-06 08:32:29 +0530

    Update MockQuestion.java

--- Excercise/MockQuestion.java
+++ Excercise/MockQuestion.java
@@ -1,23 +1,15 @@
-
 public class MockQuestion {
-
-	
-
 	public static void main(String[] args) {
-		String str="Payalpatil";
-		int tv=0;
+		String str="AshishPardeshi";
+		int telivision=0;
 		for(int i=0;i<=str.length()-1;i++) {
 			
 			for(int j=0;j<=i && tv<str.length();j++) {
-			//System.out.print(ch);
 			
-				System.out.print(str.charAt(tv));
-				tv++;
+				System.out.print(str.charAt(telivision));
+				telivision++;
 			}
-			System.out.println();
-			
-			
+			System.out.println();		
 		}
 	}
-
 }
