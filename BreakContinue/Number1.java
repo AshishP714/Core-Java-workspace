5975feea8a1cee1d366a71bbe057250279bb5681
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-14 23:19:21 +0530

    Update Number1.java
    Modify the loop to print numbers from 1 to 25.

--- BreakContinue/Number1.java
+++ BreakContinue/Number1.java
@@ -1,6 +1,4 @@
-//print the number from 1 to 50 but stop when number becomes 25 
 public class Number1 {
-
 	public static void main(String[] args) {
 		for (int i = 1; i <= 50; i++) {
 			if (i == 26) {
@@ -8,7 +6,5 @@ public class Number1 {
 			}
 			System.out.println(i);
 		}
-
 	}
-
 }
