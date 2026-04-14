e30d7f08c3be7c3eb1dd091f937fea9925597e59
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-14 23:19:54 +0530

    Update Table.java
    Modify the loop to print the table of 9, stopping when the product exceeds 50.

--- BreakContinue/Table.java
+++ BreakContinue/Table.java
@@ -1,6 +1,4 @@
-//print table of 9 but stop when product become more then 50
 public class Table {
-
 	public static void main(String[] args) {
 		for (int i = 1; i <= 10; i++) {
 			if (i * 9 > 50) {
@@ -9,5 +7,4 @@ public class Table {
 			System.out.println(i * 9);
 		}
 	}
-
 }
