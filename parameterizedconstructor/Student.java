86190714d22140edfcde612fc323bfa000f33449
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-17 23:15:56 +0530

    Remove comments

--- parameterizedconstructor/Student.java
+++ parameterizedconstructor/Student.java
@@ -1,6 +1,5 @@
 package com.parameterizedconstructor;
 
-//Write a program to create a class Student with a non-parameterized constructor that prints “Welcome Student”.
 public class Student {
 	Student() {
 		System.out.println("Welcome Student .");// non parameterized constructor
