77b4ff23d295bdc48bdca39465df38d71c7a71b4
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-17 23:16:24 +0530

    Remove comments

--- parameterizedconstructor/RectanglewithParameter.java
+++ parameterizedconstructor/RectanglewithParameter.java
@@ -1,6 +1,5 @@
 package com.parameterizedconstructor;
 
-//Create a class Rectangle with a parameterized constructor to set length and width, and print the area.
 public class RectanglewithParameter {
 	int length;
 	int width;
