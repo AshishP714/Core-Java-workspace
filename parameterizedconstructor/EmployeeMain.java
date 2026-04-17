7913a83e6cdd626a8c2762367981f78b55ff4478
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-17 23:14:59 +0530

    .

--- parameterizedconstructor/EmployeeMain.java
+++ parameterizedconstructor/EmployeeMain.java
@@ -3,11 +3,11 @@ package com.parameterizedconstructor;
 public class EmployeeMain {
 
 	public static void main(String[] args) {
-		Employee e1 = new Employee(3, "rakesh", 34000.0);
+		Employee e1 = new Employee(3, "Shubham", 35000.0);
 		System.out.println(e1);
-		Employee e2 = new Employee(9, "raju", 3700.0);
+		Employee e2 = new Employee(9, "Nikhil", 3800.0);
 		System.out.println(e2);
-		boolean a = e2.equals(e1);// check contain
+		boolean a = e2.equals(e1);
 		System.out.println(a);
 	}
 }
