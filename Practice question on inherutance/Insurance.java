4cfdef2ca4b34d08b1328d5168a020388434049c
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-18 22:07:18 +0530

    Update Insurance.java
    Removed unnecessary comments and adjusted package declaration.

--- Practice question on inherutance/Insurance.java
+++ Practice question on inherutance/Insurance.java
@@ -1,8 +1,5 @@
 package com.practice5feb;
-//9️⃣ Insurance System
-//Write a Multilevel Inheritance program for
-//Insurance → HealthInsurance → FamilyHealthPlan.
-//take input from users
+
 public class Insurance {
 public String policyNumber;
 public String policyHolderName;
