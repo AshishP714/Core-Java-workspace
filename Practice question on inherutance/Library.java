16e3bdedc12315c5fa55c8b77c1dcf998c5a6047
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-18 22:08:09 +0530

    Update Library.java

--- Practice question on inherutance/Library.java
+++ Practice question on inherutance/Library.java
@@ -1,7 +1,5 @@
 package com.practice5feb;
-//6️⃣ Library Management System
-//Write a Multilevel Inheritance program for
-//Library → Section → Book.
+
 public class Library {
 	String libName;
 	String location;
