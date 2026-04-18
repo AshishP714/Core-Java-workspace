b473e2dffe813ce93f7c094a1596720d027baf70
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-18 22:05:35 +0530

    Update Company.java

--- Practice question on inherutance/Company.java
+++ Practice question on inherutance/Company.java
@@ -1,9 +1,5 @@
 package com.practice5feb;
 
-//🔟 Company HR System
-//Create a Single Inheritance example where
-//Company is the parent class and Manager is the child class
-//take input from users
 public class Company {
 	String companyName;
 	String location;
