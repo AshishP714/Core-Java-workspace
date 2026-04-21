98571e7d8d1242ec51e01696be31deed229f1b6a
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-21 23:13:14 +0530

    Update Characters.java

--- throwexception/Characters.java
+++ throwexception/Characters.java
@@ -1,11 +1,8 @@
 package com.throwexception;
-//String Character Program
-//Create a program where a method returns a character from a String using charAt().
-//Declare throws StringIndexOutOfBoundsException and handle the exception in main()
+
 public class Characters {
 public char print(String s)throws StringIndexOutOfBoundsException {
 	char ch=s.charAt(3);
-	return ch;
-	
-}
+	return ch;	
+	}
 }
