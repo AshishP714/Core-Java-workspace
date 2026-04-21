b1d6d611d6a3e03937ff1e8ab917adc952bde3e7
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-21 23:12:44 +0530

    Update CharMain.java

--- throwexception/CharMain.java
+++ throwexception/CharMain.java
@@ -4,12 +4,9 @@ public class CharMain {
 public static void main(String[] args) {
 	Characters ch=new Characters();
 	try {
-		ch.print("Hi");
-		
-	}catch(StringIndexOutOfBoundsException e) {
+		ch.print("Hi");	
+	} catch(StringIndexOutOfBoundsException e) {
 		System.out.println("String out of index");
-		
+		}
 	}
-	
-}
 }
