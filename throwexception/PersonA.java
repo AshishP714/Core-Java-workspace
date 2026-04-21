e36f741d1f6a33ac528f071b6dc6d8a1707cbc09
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-21 23:16:02 +0530

    Remove unnecessary comments and clean up code

--- throwexception/PersonA.java
+++ throwexception/PersonA.java
@@ -1,11 +1,8 @@
 package com.throwexception;
 
 public class PersonA {
-	//throws keywords used to declare exception ,write in method signature
-	public void divide (int a,int b) throws ArithmeticException {
-		System.out.println("result ="+a/b); 
-		
-	}
 
-	
+	public void divide (int a,int b) throws ArithmeticException {
+		System.out.println("result ="+a/b); 	
+	}	
 }
