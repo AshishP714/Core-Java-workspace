4faf1d2c3c2632b03ca263512b3c807bd25c103b
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-25 21:37:58 +0530

    Refactor SquareStar1 to use nested loops for star printing
    Removed hardcoded star lines and implemented nested loops to generate a square of stars.

--- NestedForLoop/SquareStar1.java
+++ NestedForLoop/SquareStar1.java
@@ -1,11 +1,4 @@
-//* * * * *
-//* * * * *
-//* * * * *
-//* * * * *
-//* * * * *
-
 public class SquareStar1 {
-
 	public static void main(String[] args) {
 		for (int i=1;i<=5;i++) {//rows
 			for( int j=1;j<=5;j++) {//column
@@ -14,5 +7,4 @@ public class SquareStar1 {
 			System.out.println(" ");//changes line
 		}
 	}
-
 }
