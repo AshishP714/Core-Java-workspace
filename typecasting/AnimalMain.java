2ba83847f89e5c0f3104cf40c5446e02ef9a9a6b
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-28 10:36:34 +0530

    Simplify AnimalMain by removing comments
    Removed comments explaining upcasting and downcasting.

--- typecasting/AnimalMain.java
+++ typecasting/AnimalMain.java
@@ -5,12 +5,7 @@ public class AnimalMain {
 	public static void main(String[] args) {
 		Animal a = new Dog();
 		a.sound();
-		// a.bark(); not allowed because here we have makes parent class refrence so
-		// it only access parents class method
-		// this is upacasting
-		Dog d = (Dog) a; // this is downcasting in this way we can acquired
-		// properties of child class own method throght parent reference
+		Dog d = (Dog) a; 
 		d.bark();
 	}
-
 }
