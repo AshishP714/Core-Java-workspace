2f79b22bc1ca5a3afe22587f50dd8d3a94ec057f
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-28 10:37:32 +0530

    Simplify PrimitiveTypeCasting2.java by removing comments
    Removed comments explaining data loss during casting.

--- typecasting/PrimitiveTypeCasting2.java
+++ typecasting/PrimitiveTypeCasting2.java
@@ -1,11 +1,5 @@
 package com.typecasting;
 
-//✅ Question 2: Data Loss Example
-//Write a program:
-//Store int num = 150
-//Cast it into byte
-//Print both values
-//👉 Explain in comment why output changed.
 public class PrimitiveTypeCasting2 {
 
 	public static void main(String[] args) {
@@ -13,9 +7,5 @@ public class PrimitiveTypeCasting2 {
 		System.out.println("previous value " + num);
 		byte num2 = (byte) num;
 		System.out.println("After convesation " + num2);
-		// here we are storing int value in byte which comes in explicit
-		// casting where we can face data loss if we store bigger data into smaller data
-		// ofcourse data can be loss
 	}
-
 }
