4650e40b1cbd995eeeacb46279f041a2008e9e9d
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-28 10:36:10 +0530

    Clean up Animal.java by removing comments
    Removed commented instructions and package declaration.

--- typecasting/Animal.java
+++ typecasting/Animal.java
@@ -1,14 +1,5 @@
 package com.typecasting;
 
-//✅ Question 5: Upcasting Example
-//Create:
-//Class Animal → method sound()
-//Class Dog extends Animal → method bark()
-//In main:
-//Create object using upcasting
-//Animal a = new Dog();
-//Call sound()
-//👉 Try to call bark() and see error.
 public class Animal {
 	public void sound() {
 		System.out.println("Animal makes sound");
