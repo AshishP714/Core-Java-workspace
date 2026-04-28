0c4ce677ed59d5c35a2de277d791a424cb358c56
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-28 10:38:29 +0530

    Refactor DeSerialization class structure

--- Serialization and Deserialization/src/DeSerialization.java
+++ Serialization and Deserialization/src/DeSerialization.java
@@ -5,15 +5,12 @@ public class DeSerialization {
 
 	public static void main(String[] args)throws Exception {
 		
-		// deserialization process
 		FileInputStream file = new FileInputStream("C:\\study material\\employeedata.txt");
 		ObjectInputStream os = new ObjectInputStream(file);
-          Emplyoee e=(Emplyoee)os.readObject();
+        Emplyoee e=(Emplyoee)os.readObject();
 		
 		System.out.println(e.empId+" "+e.empName+" "+e.empName+" ");
 		os.close();
-		file.close();
-            
+		file.close();       
 	}
-
 }
