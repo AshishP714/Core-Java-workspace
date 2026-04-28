b4165230a8a25a7aef1ad6f93297b7669ab0404b
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-28 10:38:58 +0530

    Fix formatting and remove unnecessary lines

--- Serialization and Deserialization/src/Serialzation.java
+++ Serialization and Deserialization/src/Serialzation.java
@@ -5,14 +5,11 @@ public class Serialzation {
 
 	public static void main(String[] args) throws Exception {
 		Emplyoee e = new Emplyoee(123, "Raj", "mumbai");
-		// serialization process
 		FileOutputStream file = new FileOutputStream("C:\\study material\\employeedata.txt");
 		ObjectOutputStream os = new ObjectOutputStream(file);
 		os.writeObject(e);
 		os.close();
 		file.close();
-		System.out.println("File created Succesfully");
-		
+		System.out.println("File created Succesfully");	
 	}
-
 }
