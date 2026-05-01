7829dbc461cc4c03e3233f2dc6437357eade8439
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-01 22:43:32 +0530

    Refactor Length.java for clarity and structure

--- arraypractice/Length.java
+++ arraypractice/Length.java
@@ -1,11 +1,10 @@
 package com.arraypractice;
-//Write a program to find the length of a 1D array.
 
 import java.util.Scanner;
 
 public class Length {
 	public static void main(String[] args) {
-		// without using in-bulid method
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter integer value");
 		int[] arr1 = new int[6];
@@ -16,7 +15,6 @@ public class Length {
 		System.out.println("After taking input from users");
 		for (int i = 0; i < arr1.length; i++) {
 			length++;
-
 		}
 		System.out.println("length of 1 d array is =" + length);
 		// using method
