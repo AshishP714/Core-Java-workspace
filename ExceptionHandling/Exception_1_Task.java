2aca474e10cc2e02e003f0ec91fc0b4441a33c25
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-08 23:23:19 +0530

    Refactor Exception_1_Task.java for clarity

--- ExceptionHandling/Exception_1_Task.java
+++ ExceptionHandling/Exception_1_Task.java
@@ -1,4 +1,3 @@
-/* 1. WAP to accept number from the user and print its cube. Handle the appropriate exception if the user is entering the number in words.  */
 package ExceptionHandling;
 
 import java.util.Scanner;
@@ -6,22 +5,17 @@ import java.util.Scanner;
 public class Exception_1_Task {
 
 	public static void main(String[] args) {
-
 		Scanner scanner = new Scanner(System.in);
-		
 		try {
 			System.out.println("Enter the number : ");
 			int num = scanner.nextInt();
 			System.out.print("Cube of " + num);
 			int cube = num * num * num;
-			System.out.print(" -> " + cube);
-			
+			System.out.print(" -> " + cube);	
 		} catch (Exception e) {
 			System.out.println("Error: You are entering wrong value");
 		}
-
 		System.out.println("\nProgram will be terminated...");
-		
 		scanner.close();
 	}
 }
