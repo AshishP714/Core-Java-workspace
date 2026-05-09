d7d78e52c3fefaa615e010ce1c33a06849f93861
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-09 21:45:09 +0530

    Remove commented-out package declaration

--- Excercise/RailwaySystem.java
+++ Excercise/RailwaySystem.java
@@ -1,7 +1,5 @@
 package com.practice5feb;
 
-//design Hierarchical inheritance structure where RailwaySystem is the
-//parent class and passenger,ticket, and train are child classes.
 public class RailwaySystem {
 	public void login() {
 		System.out.println("Login into Railwayasystem page....!");
