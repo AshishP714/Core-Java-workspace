f56a09816b192887bcb5cf94ceaa096e684f2ca7
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-09 21:46:28 +0530

    Fix formatting and add newline at end of file

--- Excercise/PracticeQuestion.java
+++ Excercise/PracticeQuestion.java
@@ -1,11 +1,12 @@
 package com.string;
 
 public class PracticeQuestion {
-public static void main(String[] args) {
-	String s="Ashish";
-	String s1=new String ("Pardeshi");
-	String s2=s.concat("Pardeshi");
-	System.out.println(s==s1);
-	System.out.println(s1==s2);
-}
+	
+	public static void main(String[] args) {
+		String s="Ashish";
+		String s1=new String ("Pardeshi");
+		String s2=s.concat("Pardeshi");
+		System.out.println(s==s1);
+		System.out.println(s1==s2);
+	}
 }
