4efdc1ef918547445a6c9bfbaef2769a188ca4ed
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-13 15:12:21 +0530

    Remove unnecessary whitespace and clean up formatting

--- Inheritance/hierachicalinheritance/GoogleMain.java
+++ Inheritance/hierachicalinheritance/GoogleMain.java
@@ -3,7 +3,7 @@ package com.hierachicalinheritance;
 public class GoogleMain {
 
 	public static void main(String[] args) {
-		// create object of child class
+		
 		Gmail g1 = new Gmail();
 		g1.login();
 		g1.write();
@@ -23,5 +23,4 @@ public class GoogleMain {
 		g3.logout();
 		System.out.println("------------------------------");
 	}
-
 }
