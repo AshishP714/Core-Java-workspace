b34831df323afb3b35f4b7d8cb30688dde56d9a6
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-13 15:07:39 +0530

    Remove unnecessary whitespace and formatting

--- Excercise/DemoSession.java
+++ Excercise/DemoSession.java
@@ -5,11 +5,9 @@ public class DemoSession {
 	public static void main(String[] args) {
 		int n = 1;
 		
-		while (n > 0) {
-			
+		while (n > 0) {		
 			System.out.println(n);
 			n++;
 		}
 	}
-
 }
