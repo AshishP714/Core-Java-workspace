47e013a154d763c0e63a95175bceb447a9625c54
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-14 13:06:02 +0530

    Improve code formatting in Divisible.java

--- src/Divisible.java
+++ src/Divisible.java
@@ -2,13 +2,11 @@
 public class Divisible {
 
 	public static void main(String[] args) {
-		for(int i=1;i<=50;i++){
-			if(i%7==0) {
+		for(int i = 1; i <= 50; i++){
+			if(i % 7 == 0) {
 				continue;
 			}
 			System.out.println(i);
 		}
-
 	}
-
 }
