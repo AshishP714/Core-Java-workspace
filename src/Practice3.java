3c48bd5d9e64c82df9c36940b34abc65adfbaa21
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-14 13:12:30 +0530

    Update Practice3.java

--- src/Practice3.java
+++ src/Practice3.java
@@ -4,18 +4,15 @@ public class Practice3 {
 	public static void main(String[] args) {
 		int num = 10;// 10
 		int count = 0;
-		for (int i = 1; i <= num; i++) {// 1<=10
-			if (num % i == 0) {// 0,0,1,2,0,4,3,2,1,0 =4 count
+		for (int i = 1; i <= num; i++) {
+			if (num % i == 0) {
 				count++;
 			}
-
 		}
-		if (count == 2) {// count==3--false
+		if (count == 2) {
 			System.out.println("prime number");
 		} else {
-			System.out.println(num+" "+"is not prime number");// so this block of code execute as 10 is not prime number
+			System.out.println(num+" "+"is not prime number");
 		}
-
 	}
-
 }
