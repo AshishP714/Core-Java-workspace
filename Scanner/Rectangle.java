26b692216275ce9a4fab55597bdd42e3ce033a94
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-16 14:24:00 +0530

    Refactor Rectangle class formatting and structure

--- Scanner/Rectangle.java
+++ Scanner/Rectangle.java
@@ -2,7 +2,6 @@ package com.Scanner;
 
 import java.util.Scanner;
 
-//Take length and width and calculate area of rectangle.
 public class Rectangle {
 
 	public static void main(String[] args) {
@@ -13,6 +12,5 @@ public class Rectangle {
 		double b = sc.nextDouble();
 		System.out.println("Area of rectangle =" + a * b);
 		sc.close();
-	}
-        
+	}      
 } 
