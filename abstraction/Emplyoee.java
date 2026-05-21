adad19586780da57e14015702d79fa7b472e650f
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-21 22:15:08 +0530

    Remove commented code from Emplyoee interface

--- abstraction/Emplyoee.java
+++ abstraction/Emplyoee.java
@@ -1,11 +1,4 @@
 package com.abstraction;
-//Employee Interface
-
-//Create interface Employee
-//calculateSalary()
-//Implement for:
-//FullTimeEmployee
-//PartTimeEmployee
 
 public interface Emplyoee {
 	double calculateSalary();
