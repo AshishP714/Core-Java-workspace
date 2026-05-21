30993d19b20bb8e5b7dbd936b8992a5021cba5be
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-21 22:12:55 +0530

    Remove commented code from Calculator interface

--- abstraction/Calculator.java
+++ abstraction/Calculator.java
@@ -1,14 +1,7 @@
 package com.abstraction;
-//Calculator Interface
-
-//Create an interface Calculator with methods:
-//add()
-//subtract()
-//multiply()
-//divide()
-//Implement it in class SimpleCalculator.
 
 public interface Calculator {
+	
 	public abstract void add(int a, int b);
 
 	public abstract void multiply(int a, int b);
