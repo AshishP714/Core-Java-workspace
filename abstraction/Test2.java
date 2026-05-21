f657a7844a818d78b35cdf599251b664a744ad82
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-21 22:20:40 +0530

    Remove commented code in Test2.java

--- abstraction/Test2.java
+++ abstraction/Test2.java
@@ -3,10 +3,10 @@ package RevisionCodes;
 public class Test2 {
 
 	public static void main(String[] args) {
-		Parent.show();		// Parent static method
-		Child.show();		// Child static method
+		Parent.show();
+		Child.show();
 		
 		Parent p = new Child();
-		p.show();    // Parent static method (compile-time resolution)
+		p.show();
 	}
 }
