afbff96d3576b506404a9f9ea3e2950bcf05ad1b
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-21 22:19:39 +0530

    Refactor Shape interface by removing comments
    Removed unnecessary comments and cleaned up formatting.

--- abstraction/Shape.java
+++ abstraction/Shape.java
@@ -1,14 +1,7 @@
 package com.abstraction;
 
-//Shape Interface
-//Create interface Shape:
-//area()
-//perimeter()
-//Implement for:
-//Circle
-//Rectangle
 public interface Shape {
+	
 	double area();
-
 	double perimeter();
 }
