dcedc8261f10495475552773dcc40d83929c59c3
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-22 22:08:34 +0530

    Refactor Hotel class formatting and structure

--- Practice question on inherutance/Hotel.java
+++ Practice question on inherutance/Hotel.java
@@ -1,15 +1,10 @@
 package com.practice5feb;
 
-//7️⃣ Hotel Booking System
-//Create a Single Inheritance program where 
-//Hotel is the parent class and LuxuryHotel is the child class.
-//using encapsulation and also take input from users
 public class Hotel {
-//make private variable
+
 	private String hotelName;
 	private String hotelLocation;
 
-	// public variable
 	public void setHotelName(String hotelName) {
 		this.hotelName = hotelName;
 	}
@@ -25,5 +20,4 @@ public class Hotel {
 	public String getHotelLocation() {
 		return hotelLocation;
 	}
-
 }
