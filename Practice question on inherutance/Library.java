ae4797042686b3625979371e529d5221d683abf6
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-22 22:10:55 +0530

    Remove commented lines from Library.java

--- Practice question on inherutance/Library.java
+++ Practice question on inherutance/Library.java
@@ -1,9 +1,6 @@
 package com.practice5feb;
-//6️⃣ Library Management System
-//Write a Multilevel Inheritance program for
-//Library → Section → Book.
+
 public class Library {
 	String libName;
 	String location;
-
 }
