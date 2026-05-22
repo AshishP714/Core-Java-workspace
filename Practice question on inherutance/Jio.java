03d0dc596433ea040b6b3a441f5bed5637b91163
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-22 22:10:25 +0530

    Update Jio.java

--- Practice question on inherutance/Jio.java
+++ Practice question on inherutance/Jio.java
@@ -1,5 +1,5 @@
 package com.practice5feb;
-//create a single inheritance program where telecomService is the parent class and jio is child class
+
 public class Jio extends TelecomService {
 	public void internet() {
 		System.out.println("5G Network....!");
