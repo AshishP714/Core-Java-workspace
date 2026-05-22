f0fef76b753d88b5af2cbd16a66815f2ee72c2c9
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-22 22:15:06 +0530

    Fix formatting and remove unnecessary comments

--- Practice question on inherutance/Product.java
+++ Practice question on inherutance/Product.java
@@ -1,8 +1,5 @@
 package com.practice5feb;
 
-//3️⃣ E-Commerce Product System
-//Write a Multilevel Inheritance program for
-//Product → ElectronicProduct → Mobile.
 public class Product {
 	int productID;
 	String productName;
