e27e1a658394860cddec6dd0b4edf38e9a3e9bfd
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-24 23:04:47 +0530

    Update Bank.java

--- Excercise/Bank.java
+++ Excercise/Bank.java
@@ -3,22 +3,6 @@ package com.Excercise;
 public class Bank {
 
 	public static void main(String[] args) {
-
-//		Account account = new Account();
-//
-//		account.setName("Ashish");
-//		account.setPhone(452315469);
-//		account.setAge(22);
-//		account.setSalary(50000);
-//		account.setPin(4327);
-//		System.out.println(account.getName());
-//		System.out.println(account.getPhone());
-//		System.out.println(account.getAge());
-//		System.out.println(account.getSalary());
-//		System.out.println(account.getPin());
-		
-		
-		// Test Getters and Setters
 		
 		TestGetterSetters testGetterSetters = new TestGetterSetters();
 		
