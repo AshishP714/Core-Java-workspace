02791e96a5f44dbcc4e55d2f62e2add4d1fd885e
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-25 23:12:44 +0530

    Remove commented code and clean up DigitSumMain

--- withreturntype/DigitSumMain.java
+++ withreturntype/DigitSumMain.java
@@ -5,8 +5,5 @@ public class DigitSumMain {
 	public static void main(String[] args) {
 		DigitSum d1 = new DigitSum();
 		System.out.println(d1.addDigit());
-		// d1.addDigit(); we can write this but we do not get output
-		// as we know if you want print something on consloe you need syso to print
 	}
-
 }
