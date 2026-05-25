9642d134d919bbb67b099765606fd33880417c50
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-25 23:13:39 +0530

    Remove unnecessary comments in EvenMain.java

--- withreturntype/EvenMain.java
+++ withreturntype/EvenMain.java
@@ -5,7 +5,6 @@ public class EvenMain {
 	public static void main(String[] args) {
 		Even num = new Even();
 		System.out.println(num.checkEven());
-		// so here if num is even it will print true otherwise false
+		
 	}
-
 }
