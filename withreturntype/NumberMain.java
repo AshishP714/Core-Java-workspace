1dd272bf4b0a6dade7c67f57776927df51367e6a
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-25 23:15:24 +0530

    Remove commented code and clean up formatting

--- withreturntype/NumberMain.java
+++ withreturntype/NumberMain.java
@@ -3,12 +3,10 @@ package com.withreturntype;
 public class NumberMain {
 
 	public static void main(String[] args) {
+		
 		Number num=new Number();
-		//here in with return type and no parameter question we can store value
-		//and we can perform operation also
 		int a=num.printFixNumber();
-		a += 5;// here we are assign value in a ,a=8+5=13;
+		a += 5;
 		System.out.println(a);
 	}
-
 }
