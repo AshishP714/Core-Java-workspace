c69fc99a340bc6405d2ec2c71efc3b2a3a91f69a
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-25 23:16:27 +0530

    Remove unnecessary whitespace and formatting changes

--- withreturntype/SquareMain.java
+++ withreturntype/SquareMain.java
@@ -3,9 +3,8 @@ package com.withreturntype;
 public class SquareMain {
 
 	public static void main(String[] args) {
-		//create a object to call method
+		
 		Square s1=new Square();
 		System.out.println("square of "+s1.printSquare());
 	}
-
 }
