4ff04e627b16ce619124927aa967f6f422c4b753
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-25 23:14:56 +0530

    Remove unnecessary package declaration and comments

--- withreturntype/Number.java
+++ withreturntype/Number.java
@@ -1,7 +1,5 @@
 package com.withreturntype;
 
-//No Parameter WITH Return Type
-//Write a method that returns an integer representing a fixed number.
 public class Number {
 	public int printFixNumber() {
 		int a = 8;
