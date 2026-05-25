84b3458b56b0c1e8085a3a8b61581685fe8f81e3
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-25 23:15:59 +0530

    Refactor Square class formatting and structure

--- withreturntype/Square.java
+++ withreturntype/Square.java
@@ -1,8 +1,9 @@
 package com.withreturntype;
-//Write a method that return the square of 5
+
 public class Square {
-public int printSquare() {
-	int a=5;
-	return a*a;
-}
+	
+	public int printSquare() {
+		int a=5;
+		return a*a;
+	}
 }
