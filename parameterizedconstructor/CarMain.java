8a448903caca300de511973093fed06e76b92618
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-26 20:19:51 +0530

    Update CarMain.java

--- parameterizedconstructor/CarMain.java
+++ parameterizedconstructor/CarMain.java
@@ -3,9 +3,8 @@ package com.parameterizedconstructor;
 public class CarMain {
 
 	public static void main(String[] args) {
-		Car c1 = new Car();// non parameterized constructor
-		Car c2 = new Car("TATA", 780000.0);//parameterized constructor
+		Car c1 = new Car();
+		Car c2 = new Car("TATA", 780000.0);
 		System.out.println(c2);
 	}
-
 }
