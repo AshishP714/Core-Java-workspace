d2d53c3922961532cdc23869c18a7b113e2bdcfc
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-27 22:59:44 +0530

    Update Student.java

--- parameterizedconstructor/Student.java
+++ parameterizedconstructor/Student.java
@@ -2,6 +2,6 @@ package com.parameterizedconstructor;
 
 public class Student {
 	Student() {
-		System.out.println("Welcome Student .");// non parameterized constructor
+		System.out.println("Welcome Student .");
 	}
 }
