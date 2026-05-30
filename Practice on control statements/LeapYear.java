a767b4ac8122704f3678b153db1ef1d86ac74f50
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-05-30 23:21:22 +0530

    Update LeapYear.java

--- Practice on control statements/LeapYear.java
+++ Practice on control statements/LeapYear.java
@@ -1,5 +1,5 @@
-//write a program to accept a year check it is a leap year or not
 public class LeapYear {
+	
 	public static void main(String[] args) {
 		int year = 2007;
 		if ((year % 4 == 0 && year % 100 != 0 )|| (year % 400 == 0)) {
@@ -7,7 +7,5 @@ public class LeapYear {
 		} else {
 			System.out.println("it is not leap year");
 		}
-
 	}
-
 }
