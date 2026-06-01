2d7dd41aa11d8d09c65cf573c25157eb6a450fde
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-01 23:44:14 +0530

    Fix formatting and add newline at end of file

--- arraypractice/Declareinitialize.java
+++ arraypractice/Declareinitialize.java
@@ -1,15 +1,16 @@
 package com.arraypractice;
 
 public class Declareinitialize {
-public static void main(String[] args) {
-	int[] arr=new int[4];//dynamic way
-	arr[0]= 5;
-	arr[1]= 10;
-	arr[2]= 15;
-	arr[3]= 20;
-	System.out.println(arr[1]);
 	
-	int[] arr1= {67,89,896};
-	System.out.println(arr1[2]);
+	public static void main(String[] args) {
+		int[] arr=new int[4];
+		arr[0]= 5;
+		arr[1]= 10;
+		arr[2]= 15;
+		arr[3]= 20;
+		System.out.println(arr[1]);
+		
+		int[] arr1= {67,89,896};
+		System.out.println(arr1[2]);
+		}
 	}
-}
