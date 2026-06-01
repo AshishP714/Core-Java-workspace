d7ddf3ee0f913390aaa68d5505e61ed20e70ce91
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-01 23:41:41 +0530

    Refactor CountElementsInArray class structure

--- Array/CountElementsInArray.java
+++ Array/CountElementsInArray.java
@@ -3,8 +3,6 @@ package Array;
 import java.util.Scanner;
 
 public class CountElementsInArray {
-
-	
 	public int countElement(int array[]) {
 		int sum = 0;
 		for (int i = 0; i < array.length; i++) {
@@ -12,18 +10,15 @@ public class CountElementsInArray {
 		}
 		return sum;
 	}
+	
 	public static void main(String[] args) {
-		
 		Scanner scanner = new Scanner(System.in);
-		
 		int arr[] = new int[10];
 		System.out.println("Enter array element :- ");
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = scanner.nextInt();
 		}
-		
 		int result = new CountElementsInArray().countElement(arr);
-		
 		System.out.println(result);
 	}
 }
