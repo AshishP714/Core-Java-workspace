56acce7dfdfd6a26cd0145c2838b91fca9348cde
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-05 21:22:21 +0530

    Refactor Mall class structure and formatting

--- statickeyword/Mall.java
+++ statickeyword/Mall.java
@@ -1,27 +1,16 @@
 package com.statickeyword;
 
-//🔹 Question 1 – Visitor Counter 🏢
-//Create a class Mall:
-//mallName (same for all)
-//visitorName (different for each)
-//totalVisitors (should increase every time object is created)
 public class Mall {
-//practice on static variable
 	static String mallName;// static variable
 	String visitorName;// non static variable
 	static int totalVisistors = 0;
-	// non static blocks
 	{
 		mallName = "Global mall";
 	}
-
-	// constructor
 	Mall(String visitorName) {
 		this.visitorName = visitorName;
 		totalVisistors++;
 	}
-
-	// method
 	public void display() {
 		System.out.println("Mall name =" + mallName + "---" + "Visitor name =" + visitorName);
 	}
@@ -32,6 +21,5 @@ public class Mall {
 		Mall m1 = new Mall("riya");
 		m1.display();
 		System.out.println("Total visistor =" + totalVisistors);
-
 	}
 }
