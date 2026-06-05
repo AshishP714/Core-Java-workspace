12b514aad8b0f02b011921089ddc8a9a2969073a
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-05 21:24:45 +0530

    Remove commented code and clean up B.java

--- statickeyword/B.java
+++ statickeyword/B.java
@@ -1,7 +1,6 @@
 package com.statickeyword;
 
 public class B {
-//by making static method 
 	public static void method1() {
 		System.out.println("This is method1 from static method");
 	}
@@ -16,12 +15,10 @@ public class B {
 	}
 
 	public static void main(String[] args) {
-		B.method1();// no need to create object
+		B.method1();
 		int a = B.add(3, 9);
 		System.out.println("Sum =" + a);
-		//B.display(); this is not allowed to call
 		B b=new B();
-		b.display();//for to call non static method we need to create object creation
-
+		b.display();
 	}
 }
