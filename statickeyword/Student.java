5fb14ae8a3879529f11a9057cd5b3aeea99cf86e
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-05 21:18:09 +0530

    Refactor Student class and remove commented code

--- statickeyword/Student.java
+++ statickeyword/Student.java
@@ -1,34 +1,20 @@
 package com.statickeyword;
 
-//🔹 Question 2 – School Exam System 🏫
-//Create a class Student:
-//name
-//marks
-//passingMarks (same for all students)
-//totalStudents (count total objects)
-//Print passing marks and total students.
 public class Student {
-	// variables non static and static
 	String name;
 	int marks;
 	static int passingMarks;
 	static int totalStudents = 0;
-//non static block
 	{
 		passingMarks = 35;
 	}
-
-//constructor
 	Student(String name, int marks) {
 		this.name = name;
 		this.marks = marks;
 		totalStudents++;
 	}
-
-	// non static method
 	public void display() {
 		System.out.println("Student name =" + name + " , " + "marks =" + marks);
-		// System.out.println("Passing marks =" + passingMarks);
 	}
 
 	public static void main(String[] args) {
