0ec482e309c02755375c238e1271b7ccd59c660a
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-09 22:20:00 +0530

    Format and clean up Mix.java code

--- string/Mix.java
+++ string/Mix.java
@@ -1,27 +1,27 @@
 package com.string;
 
 public class Mix {
+	
 	public static void main(String[] args) {
 		String s1 = new String("Ashish");
 		String s2 = "Ashish";
 		String s3 = s1;
-		// checking content first
-		System.out.println(s1.equals(s2));// true
-		System.out.println(s2.equals(s3));// true
-		// checking reference
-		System.out.println(s1 == s2);// false
-		System.out.println(s3 == s1);// true
-		System.out.println(s1.hashCode());// same
+		
+		System.out.println(s1.equals(s2));
+		System.out.println(s2.equals(s3));
+		
+		System.out.println(s1 == s2);
+		System.out.println(s3 == s1);
+		System.out.println(s1.hashCode());
 		System.out.println(s3.hashCode());
-		System.out.println(System.identityHashCode(s1));// same
+		System.out.println(System.identityHashCode(s1));
 		System.out.println(System.identityHashCode(s3));
-		System.out.println(System.identityHashCode(s2));// different
-		// immutable (Can not changed)
+		System.out.println(System.identityHashCode(s2));
+		
 		String str1 = "Ashish";
 		String str2 = str1.concat(" Pardeshi");
 		System.out.println(str1);
-		System.out.println(str2);// it store in different object in heap
-		System.out.println(str1 == str2);// false pointing differnt object
-
+		System.out.println(str2);
+		System.out.println(str1 == str2);
 	}
 }
