93e832c93c4071ef83dec5651e90bf636a0a4482
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-11 22:55:58 +0530

    Remove comments from Revese.java

--- comm/pracctice/Revese.java
+++ comm/pracctice/Revese.java
@@ -1,17 +1,14 @@
 package comm.pracctice;
-//1. Reverse a String
-//
-//Input: "hello"
-//Output: "olleh"
+
 public class Revese {
-public static void main(String[] args) {
-	String str="hello";
-	String str2="";
-	for(int i=str.length()-1;i>=0;i--) {
-		char ch=str.charAt(i);
-		str2=str2+ch;
-		
+	
+	public static void main(String[] args) {
+		String str="hello";
+		String str2="";
+		for(int i=str.length()-1;i>=0;i--) {
+			char ch=str.charAt(i);
+			str2=str2+ch;
+		}
+		System.out.println(str2);
 	}
-	System.out.println(str2);
-}
 }
