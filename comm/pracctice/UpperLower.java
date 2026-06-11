76a282b6af0c83657fc73676a2c31df287e03599
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-11 22:54:59 +0530

    Change input string to 'ASHISH' in UpperLower.java

--- comm/pracctice/UpperLower.java
+++ comm/pracctice/UpperLower.java
@@ -1,10 +1,8 @@
 package comm.pracctice;
-//8. Convert Uppercase to Lowercase Without Built-in Method
-//Input: "HELLO"
-//Output: "hello"
+
 public class UpperLower {
 public static void main(String[] args) {
-	String str="HELLO";
+	String str="ASHISH";
 	String str1="";
 	for(int i=0;i<=str.length()-1;i++) {
 		char ch=str.charAt(i);
@@ -12,11 +10,8 @@ public static void main(String[] args) {
 			str1=str1+(char)(ch+32);
 		}else {
 			str1=str1+(char)(ch+32);
-		}
-	
-		
+		}	
 	}
 	System.out.println(str1);
-	
-}
+	}
 }
