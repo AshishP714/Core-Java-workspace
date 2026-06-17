b48883c00edab67ea328013a747b60c084c0fa47
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-17 23:48:06 +0530

    Refactor Fibonacci series printing code

--- src/Practice.java
+++ src/Practice.java
@@ -1,17 +1,15 @@
-//print fibonacci series from 1 to 50 
-public class Practice {
+ public class Practice {
 
 	public static void main(String[] args) {
 		int a=0;
 		int b=1;
-		System.out.println(a);//0
-		System.out.println(b);//1
+		System.out.println(a);
+		System.out.println(b);
 		for( int i=1;i<=48;i++) {
-			int c=a+b;//c=0+1=1,1+1=2
-			a=b;//a=1 ,b=1  a=1,b=2
+			int c=a+b;
+			a=b;
 			b=c;
 			System.out.println(c);//1//2
-		}
-		
+		}		
 	}
 }
