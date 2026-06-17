81d6ba4a0491d57e9170eb66fe49e106ceeba927
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-17 23:50:04 +0530

    Refactor Alphabet class to maintain functionality

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -1,7 +1,7 @@
-//print characters from A to Z but skip vowels
 public class Alphabet {
 
 	public static void main(String[] args) {
+		
 		for(char ch='A';ch<='Z';ch++) {
 			if(ch=='A'||ch=='E'||ch=='I'||ch=='O'|| ch=='U') {
 			continue;	
