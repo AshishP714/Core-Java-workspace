973bffb504905528ac9cbb165ac2ea90063822f8
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-19 23:41:55 +0530

    Fix formatting and remove unnecessary lines

--- TernaryOperator1.java
+++ TernaryOperator1.java
@@ -2,14 +2,11 @@ package Exercise;
 
 public class TernaryOperator1 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		int a = 10;
 		int b = 30;
 		int c = 40;
-		
 		int sum = (a > b)?(b > c ? a : c):(b > c ? b : c);
-		
 		System.out.println(sum);
 	}
 } 
