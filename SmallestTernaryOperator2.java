fa607f8f666f24f327e730255bfcdc590880d6d2
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-19 23:41:09 +0530

    Remove unnecessary package and clean up code

--- SmallestTernaryOperator2.java
+++ SmallestTernaryOperator2.java
@@ -3,13 +3,10 @@ package Exercise;
 public class SmallestTernaryOperator2 {
 
 	public static void main(String[] args) {
-
 		int a = 10;
 		int b = 30;
 		int c = 40;
-
 		int min = (a < b) ? (a < c ? a : c) : (b < c ? b : c);
-		
 		System.out.println(min);
 	}
 }
