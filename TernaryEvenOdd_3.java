ecc02091a4cd130f8f76d340c9caecccb0f517fe
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-19 23:40:33 +0530

    Remove unnecessary package declaration

--- TernaryEvenOdd_3.java
+++ TernaryEvenOdd_3.java
@@ -2,12 +2,9 @@ package Exercise;
 
 public class TernaryEvenOdd_3 {
 
-	public static void main(String[] args) {
-		
-		int num = 5;
-		
+	public static void main(String[] args) {	
+		int num = 5;		
 		String result = (num % 2 == 0) ? "Even" : "Odd";
-		
 		System.out.println(result);
 	}
 }
