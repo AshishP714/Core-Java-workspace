1a1d17c9a39ba32b9951cce3561c923c230e8145
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-29 23:43:18 +0530

    Fix formatting and structure in UpperLower.java

--- comm/pracctice/UpperLower.java
+++ comm/pracctice/UpperLower.java
@@ -1,7 +1,9 @@
 package comm.pracctice;
 
-public class UpperLower {
+public class UpperLower 
+
 public static void main(String[] args) {
+	
 	String str="ASHISH";
 	String str1="";
 	for(int i=0;i<=str.length()-1;i++) {
