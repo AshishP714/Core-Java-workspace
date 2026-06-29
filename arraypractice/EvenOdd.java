77892764acf42b73261c296d6eef96cad3458997
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-29 23:49:37 +0530

    Fix formatting and add newline at end of EvenOdd.java

--- arraypractice/EvenOdd.java
+++ arraypractice/EvenOdd.java
@@ -1,12 +1,14 @@
 package com.arraypractice;
 
 public class EvenOdd {
+	
 public static void main(String[] args) {
+	
 	int[] arr= {37,89,12,45,99,34};
 	for(int i=0;i<arr.length;i++) {
 		if(arr[i]%2==0) {
 			System.out.println("Even number"+arr[i]);
-		}else if(arr[i]%2!=0){
+		} else if(arr[i]%2!=0){
 			System.out.println("Odd number ="+arr[i]);
 			}
 		}
