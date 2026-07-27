ba4fa0b4b82ec789d6f96eee74a579d1450e7692
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-07-27 21:30:42 +0530

    Remove unnecessary comments and clean up code

--- string/ByObjectCreation.java
+++ string/ByObjectCreation.java
@@ -2,12 +2,12 @@ package com.string;
 
 public class ByObjectCreation {
 	public static void main(String[] args) {
-		//String with new 
+		
 		String s1=new String("Ashish");
 		String s2=new String("Ashish");
 		String s3=new String("Shubham");
 		System.out.println("by checking equal() method....!");
-		//first we will check if they have content or not
+		
 		System.out.println(s1.equals(s2));
 		System.out.println(s1.equals(s3));
 		System.out.println(s2.equals(s3));//false
@@ -18,7 +18,7 @@ public class ByObjectCreation {
 		String s4=s3;
 		System.out.println(s3==s4);//pointing in same object true
 		System.out.println("by checking hashcode method");
-		//hashcode is method which return int value if both object contain same then hashcode will be same for both refrence 
+		
 		System.out.println(s1.hashCode());//same
 		System.out.println(s2.hashCode());//same
 		
