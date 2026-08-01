679652cfe58a49e7cf7f97b434f39cd672e03eb3
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-08-01 23:42:05 +0530

    Refactor exception handling in PropagationExample
    Updated method3 to demonstrate exception handling in method1.

--- PropagationExample.java
+++ PropagationExample.java
@@ -1,16 +1,16 @@
 public class PropagationExample {
 
     void method3() {
-        int result = 50 / 0; // ❌ Yahan ArithmeticException aayi (Divide by zero)
+        int result = 50 / 0;
     }
 
     void method2() {
-        method3(); // method3 ko call kiya, par handle nahi kiya -> Propagate hui
+        method3();
     }
 
     void method1() {
         try {
-            method2(); // method2 ko call kiya aur try-catch mein daal diya
+            method2();
         } catch (Exception e) {
             System.out.println("Exception Handle Ho Gayi method1 mein!");
         }
