3938a213c341b6fc797527da34aedbc41ed75200
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-08-02 09:11:45 +0530

    Add Student class with id and name fields

---
+++ pracctice/Student.java
@@ -0,0 +1,15 @@
+package comm.pracctice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+class Student {
+    int id;
+    String name;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
