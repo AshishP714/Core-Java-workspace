026292bd09d0b7c10be6b1e4508cc9151f7030f2
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-08-02 09:12:40 +0530

    Fix formatting issue in NameComparator.java

---
+++ pracctice/NameComparator.java
@@ -0,0 +1,8 @@
+package comm.pracctice;
+
+class NameComparator implements Comparator<Student> {
+    @Override
+    public int compare(Student s1, Student s2) {
+        return s1.name.compareTo(s2.name);
+    }
+}
