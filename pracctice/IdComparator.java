2c6a94843863cdaba32419167f6d00eb4afdce53
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-08-02 09:13:31 +0530

    Add IdComparator class for comparing Student IDs

---
+++ pracctice/IdComparator.java
@@ -0,0 +1,8 @@
+package comm.pracctice;
+
+class IdComparator implements Comparator<Student> {
+    @Override
+    public int compare(Student s1, Student s2) {
+        return s1.id - s2.id;
+    }
+}
